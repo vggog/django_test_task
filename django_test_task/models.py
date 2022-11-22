7af77b5037355729ff0d6c2b19894dac633db742
vggog <gluhovkich@gmail.com>
2022-11-23 03:32:35 +0500

    Add price_id collumn and change price collumn

--- django_test_task/models.py
+++ django_test_task/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=10)
+    # In the database the price is stored in cents.
+    price = models.IntegerField()
+    price_id = models.TextField()
